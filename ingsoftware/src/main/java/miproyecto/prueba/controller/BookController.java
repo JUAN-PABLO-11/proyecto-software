1b7f7724f90ab6327c8effbd04da3422046aa7c4
pipecm05 <andresf.garciac1@uqvirtual.edu.co>
2026-09-21 14:20:24 -0500

    punto2

--- ingsoftware/src/main/java/miproyecto/prueba/controller/BookController.java
+++ ingsoftware/src/main/java/miproyecto/prueba/controller/BookController.java
@@ -1,4 +1,30 @@
 package miproyecto.prueba.controller;
 
+import miproyecto.prueba.model.Book;
+import miproyecto.prueba.service.BookService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
 public class BookController {
+
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("/search")
+    public List<Book> searchBooks(
+            @RequestParam(required = false) String author,
+            @RequestParam(required = false) String title,
+            @RequestParam(required = false) String isbn) {
+
+        return bookService.searchBooks(author, title, isbn);
+    }
 }
--- ingsoftware/src/main/java/miproyecto/prueba/service/BookService.java
+++ ingsoftware/src/main/java/miproyecto/prueba/service/BookService.java
@@ -1,4 +1,21 @@
 package miproyecto.prueba.service;
 
+import miproyecto.prueba.model.Book;
+import miproyecto.prueba.repository.BookRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class BookService {
+
+    private final BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public List<Book> searchBooks(String author, String title, String isbn) {
+        return bookRepository.searchBooks(author, title, isbn);
+    }
 }
